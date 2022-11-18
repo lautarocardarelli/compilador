0bac91723489bff0493e0fa2009dd31f86ccb579
Lautaro Cardarelli <lauti.cardarelli@gmail.com>
2022-11-18 17:13:31 -0300

    update symbol table generator

--- src/main/java/lyc/compiler/files/SymbolTableGenerator.java
+++ src/main/java/lyc/compiler/files/SymbolTableGenerator.java
@@ -1,9 +1,58 @@
 package lyc.compiler.files;
+import lyc.compiler.model.Simbolo;
+
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Formatter;
+import java.util.HashSet;
+import java.util.Set;
 
 public class SymbolTableGenerator implements FileGenerator {
+
+    Set<Simbolo> simbolos = new HashSet<Simbolo>();
+    public void save(String tipo, String valor) {
+        switch (tipo) {
+            case "ID":
+                simbolos.add(new Simbolo(valor, tipo));
+                break;
+            case "CTE_STRING":
+                simbolos.add(new Simbolo("_" + valor, tipo, valor, Integer.toString(valor.length() - 2)));
+                break;
+            case "CTE_INTEGER":
+            case "CTE_FLOAT":
+                simbolos.add(new Simbolo("_" + valor, tipo, valor));
+                break;
+            default:
+                break;
+        }
+    }
     @Override
     public void generate(FileWriter fileWriter) throws IOException {
+        try {
+            String ruta = "./target/symbol-table.txt";
+            File file = new File(ruta);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fw);
+            Formatter fmt = new Formatter();
+            fmt.format("%15s %15s %15s %15s\n", "Nombre", "Tipo", "Valor", "Longitud");
+            this.simbolos.forEach(simbolo -> {
+                try {
+                    fmt.format("%15s %15s %15s %15s\n", simbolo.getNombre(), simbolo.getTipo(), simbolo.getValor(), simbolo.getLongitud());
+                }
+                catch (Exception ex) {
+
+                }
+
+            });
+            bw.write(String.valueOf(fmt));
+            bw.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
