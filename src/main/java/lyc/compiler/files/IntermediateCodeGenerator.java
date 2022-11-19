601e83c80b2ec5bd0d92ba3b4b0cfdf36822e6e2
Lautaro Cardarelli <lauti.cardarelli@gmail.com>
2022-11-19 14:07:11 -0300

    add comparator and branch logic to icg

--- src/main/java/lyc/compiler/files/IntermediateCodeGenerator.java
+++ src/main/java/lyc/compiler/files/IntermediateCodeGenerator.java
@@ -23,7 +23,42 @@ public class IntermediateCodeGenerator implements FileGenerator {
     }
     /* Fin Singleton */
 
+    /* Para tipar Ids */
     private ArrayList<String> listIdsToAddType = new ArrayList<String>();
+
+    public void setVariableType(String variableType) {
+        this._variableType = variableType;
+    }
+
+    private String _variableType = "";
+
+    /* Para condiciones y Saltos */
+
+    /* rename */
+    private Integer _saltoOr = null;
+
+    public void setOpLogico(String opLogico) {
+        this._opLogico = opLogico;
+    }
+
+    private String _opLogico = "";
+
+    public void setComparador(String comparador) {
+        this._comparador = comparador;
+    }
+
+    private String _comparador = "";
+
+    private Stack<Object> stackSaltoFuera = new Stack<Object>();
+    private Stack<Object> stackSaltoDentro = new Stack<Object>();
+
+    private Stack<Object> stackSaltoFinal = new Stack<Object>();
+
+    public void invertCurrentComparador() {
+        _comparador = getInvertedComparator(_comparador);
+    }
+    /* Fin condicones y saltos */
+
     private SymbolTableGenerator stg;
     private static Integer celdaActual = 0;
     private static Stack<Integer> cellStack = new Stack<>();
@@ -116,12 +151,85 @@ public class IntermediateCodeGenerator implements FileGenerator {
         listIdsToAddType.add(id);
     }
 
-    public void addTypeToIds(String type) {
+    public void addTypeToIds() {
         listIdsToAddType.forEach(id -> {
-            stg.updateIdType(id, type);
+            stg.updateIdType(id, _variableType);
         });
 
         listIdsToAddType.clear();
+        _variableType = "";
+    }
+    
+    public void insertarComparadores() {
+        insertarEnPolaca("CMP");
+        if (_opLogico == "AND") {
+            insertarEnPolaca(getInvertedComparator(_comparador));
+            /* Salto al false, bloque false */
+            pushCurrentCellAndGo(stackSaltoFuera);
+        } else {
+            insertarEnPolaca(_comparador);
+            pushCurrentCellAndGo(stackSaltoDentro);
+        }
+    }
+
+
+    /* IF SALTOS*/
+
+    public void IFSalto() {
+        if (_opLogico == "OR") {
+            while (!stackSaltoDentro.empty()) {
+                Integer celda = Integer.parseInt(stackSaltoDentro.pop().toString());
+
+                /* Extraer a un metodo */
+                polaca.add(celda, Integer.toString(polaca.size() + 2));
+
+                insertarEnPolaca("BI");
+                _saltoOr = celdaActual;
+                celdaActual ++;
+
+            }
+        }
+
+        if (_opLogico == "") {
+            insertarEnPolaca("CMP");
+            insertarEnPolaca(getInvertedComparator(_comparador));
+            stackSaltoFuera.add(celdaActual);
+            celdaActual ++;
+        }
+    }
+
+    public void IFProgram() {
+        insertarEnPolaca("BI");
+        stackSaltoFinal.add(celdaActual);
+        celdaActual ++;
+    }
+
+    public void IFSaltoElse () {
+        if (_opLogico == "AND") {
+            while (!stackSaltoFuera.empty()) {
+                Integer celda = Integer.parseInt(stackSaltoFuera.pop().toString());
+                polaca.add(celda, celdaActual.toString());
+            }
+        }
+
+        if (_opLogico == "OR") {
+            polaca.add(_saltoOr, celdaActual.toString());
+
+        }
+
+        if (_opLogico == "") {
+            while (!stackSaltoFuera.empty()) {
+                Integer celda = Integer.parseInt(stackSaltoFuera.pop().toString());
+                polaca.add(celda, celdaActual.toString());
+            }
+        }
+    }
+
+    public void IFSaltoFinal() {
+        while (!stackSaltoFinal.empty()) {
+            Integer celda = Integer.parseInt(stackSaltoFinal.pop().toString());
+            polaca.add(celda, celdaActual.toString());
+        }
     }
 }
 
