41581b4a6600b9fbcbe43cb0732067e0b65a8863
Lautaro Cardarelli <lauti.cardarelli@gmail.com>
2022-11-24 19:26:58 -0300

    generate header asm

--- src/main/java/lyc/compiler/files/AsmCodeGenerator.java
+++ src/main/java/lyc/compiler/files/AsmCodeGenerator.java
@@ -2,11 +2,37 @@ package lyc.compiler.files;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Formatter;
 
 public class AsmCodeGenerator implements FileGenerator {
 
+    IntermediateCodeGenerator icg = IntermediateCodeGenerator.getIcgInstance();
+    SymbolTableGenerator stg = SymbolTableGenerator.getStgInstance();
     @Override
     public void generate(FileWriter fileWriter) throws IOException {
-        fileWriter.write("TODO");
+        fileWriter.write("include macros2.asm\n");
+        fileWriter.write("include number.asm\n");
+        fileWriter.write("\n");
+
+        fileWriter.write(".MODEL \t LARGE\n");
+        fileWriter.write(".386 \n");
+        fileWriter.write(".STACK 200h \n");
+
+        fileWriter.write("\n");
+        fileWriter.write(".DATA\n");
+
+        Formatter fmt = new Formatter();
+        stg.simbolos.forEach(simbolo -> {
+            String valor;
+            if (simbolo.tipo == "ID") {
+                valor = "?";
+            } else {
+                valor = simbolo.getValor();
+            }
+            fmt.format("%15s %15s %15s\n", simbolo.getNombre(), "dd", valor);
+        });
+
+        fileWriter.write(String.valueOf(fmt));
+        fileWriter.write(".CODE\n");
     }
 }
